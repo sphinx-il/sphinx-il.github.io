548fa67550e915ca49ee74405a1670c8de8da6c9
sphinx-il <sphinxbot24@gmail.com>
2024-10-14 10:14:17 -0700

    bar charts

--- static/js/index.js
+++ static/js/index.js
@@ -65,25 +65,26 @@ function handleBarClick(event, chartElements) {
 
   // List of video sources
   const videoSources = [
+    // OpenVLA
+    "media/table1/openvla_cups.mp4",
+    "media/table1/openvla_train.mp4",
+    "media/table1/openvla_coffee.mp4",
+    // Hydra
+    "media/table1/hydra_cups.mp4",
+    "media/table1/hydra_train.mp4",
+    "media/table1/hydra_coffee.mp4",
     // DP
-    "media/table1/dp_fold.mp4",
-    "media/table1/dp_fork.mp4",
-    "media/table1/dp_serve.mp4",
-    "media/table1/dp_socks.mp4",
-    // ACT
-    "media/table1/act_fold.mp4",
-    "media/table1/act_fork.mp4",
-    "media/table1/act_serve.mp4",
-    "media/table1/act_socks.mp4",
-    // MT-π (H+R)
-    "media/table1/mt_pi_fold.mp4",
-    "media/table1/mt_pi_fork.mp4",
-    "media/table1/mt_pi_serve.mp4",
-    "media/table1/mt_pi_socks.mp4",
+    "media/table1/dp_cups.mp4",
+    "media/table1/dp_train.mp4",
+    "media/table1/dp_coffee.mp4",
+    // SPHINX
+    "media/table1/sphinx_cups.mp4",
+    "media/table1/sphinx_train.mp4",
+    "media/table1/sphinx_coffee.mp4",
   ];
 
   // Calculate the video index
-  const videoIndex = datasetIndex * 4 + index;
+  const videoIndex = datasetIndex * 3 + index;
   const videoSrc = videoSources[videoIndex];
 
   // Select the existing video container
@@ -122,23 +123,30 @@ function generateBarChart() {
   const ctx = document.getElementById('taskBarChart').getContext('2d');
 
   const data = {
-      labels: ['Fold Cloth', 'Fork on Plate', 'Serve Egg', 'Put Away Socks'],
+      labels: ['Cup Stack', 'Train Track', 'Coffee Making'],
       datasets: [
           {
-              label: 'DP',
-              data: [7/20 * 100, 3/20 * 100, 7/20 * 100, 10/20 * 100], 
-              backgroundColor: '#bfbfbf',
+              label: 'OpenVLA',
+              data: [2/20 * 100, 0/20 * 100, 0/20 * 100], 
+              backgroundColor: '#60b0db',
               borderWidth: 1
           },
           {
-              label: 'ACT',
-              data: [14/20 * 100, 5/20 * 100, 3/20 * 100, 11/20 * 100],
-              backgroundColor: '#b8cacc',
+              label: 'HYDRA',
+              data: [2/20 * 100, 1/20 * 100, 3/20 * 100], 
+              backgroundColor: '#90b584',
               borderWidth: 1
           },
           {
-              label: 'MT-π (H+R)',
-              data: [18/20 * 100, 18/20 * 100, 17/20 * 100, 16/20 * 100],
+              label: 'Diffusion Policy',
+              data: [9/20 * 100, 0/20 * 100, 7/20 * 100], 
+              backgroundColor: '#666e64',
+              borderWidth: 1
+          },
+
+          {
+              label: 'SPHINX',
+              data: [19/20 * 100, 16/20 * 100, 14/20 * 100],
               backgroundColor: '#f5a802', 
               borderWidth: 1
           }
@@ -243,6 +251,144 @@ function generateBarChart() {
   new Chart(ctx, config);
 }
 
+
+// Bar chart generation function (partially modified)
+function generateWaypointBarChart() {
+  const ctx = document.getElementById('taskBarChartWaypoint').getContext('2d');
+
+  const data = {
+      labels: ['Robomimic Can', 'Robomimic Square', 'Drawer'],
+      datasets: [
+          {
+              label: 'Diffusion Policy',
+              data: [67.5, 44.0, 65.0], 
+              backgroundColor: '#666e64',
+              borderWidth: 1
+          },
+          {
+              label: '3D Diffusion Policy',
+              data: [56.0, 29.0, 65.0], 
+              backgroundColor: '#343833',
+              borderWidth: 1
+          },
+          {
+              label: 'Vanilla Waypoint',
+              data: [70.0, 13.0, 40.0], 
+              backgroundColor: '#63543f',
+              borderWidth: 1
+          },
+          {
+              label: 'Vanilla Waypoint (Aux Salient Points)',
+              data: [78.0, 65.5, 55.0], 
+              backgroundColor: '#ada08c',
+              borderWidth: 1
+          },
+          {
+              label: 'SPHINX',
+              data: [93.5, 86.5, 95.0],
+              backgroundColor: '#f5a802', 
+              borderWidth: 1
+          }
+      ]
+  };
+
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                max: 100,
+                ticks: {
+                    stepSize: 20,
+                    color: 'black',
+                    font: {
+                        size: 18,
+                        weight: 'bold',
+                        family: 'Palatino, serif'
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Success Rate (%)',
+                    color: 'black',
+                    font: {
+                        size: 20,
+                        weight: 'bold',
+                        family: 'Palatino, serif'
+                    }
+                },
+                grid: {
+                    display: false
+                },
+                border: {
+                    display: true
+                },
+                borderWidth: 2
+            },
+            x: {
+                ticks: {
+                    color: 'black',
+                    font: {
+                        size: 18,
+                        weight: 'bold',
+                        family: 'Palatino, serif'
+                    }
+                },
+                title: {
+                    display: true,
+                    text: 'Tasks',
+                    color: 'black',
+                    font: {
+                        size: 20,
+                        weight: 'bold',
+                        family: 'Palatino, serif'
+                    }
+                },
+                grid: {
+                    display: false
+                },
+                border: {
+                    display: true
+                },
+            },
+        },
+        plugins: {
+            legend: {
+                display: true,
+                position: 'top',
+                labels: {
+                    font: {
+                        size: 18,
+                        weight: 'bold',
+                        family: 'Palatino, serif'
+                    },
+                    color: 'black'
+                }
+            },
+            tooltip: {
+              callbacks: {
+                title: (tooltipItems) => {
+                  return `Task: ${tooltipItems[0].label}`;
+                },
+                label: (context) => {
+                  return `${context.dataset.label}: ${context.parsed.y.toFixed(2)}%`;
+                }
+              }
+            }
+        },
+        elements: {
+            bar: {
+                borderRadius: 4
+            }
+        }
+    }
+  };
+
+  new Chart(ctx, config);
+}
+
 // Main initialization function
 function init() {
   $(".navbar-burger").click(function() {
@@ -266,12 +412,12 @@ function init() {
     generateBarChart(); // Ensure this is called after the canvas is in the DOM
   }
 
+  if (document.getElementById('taskBarChartWaypoint')) {
+    generateWaypointBarChart(); // Ensure this is called after the canvas is in the DOM
+  }
+
   // Display initial message in the video container
   const videoContainer = document.getElementById('video-container');
-  const initialMessage = document.createElement('div');
-  initialMessage.className = 'initial-message';
-  initialMessage.innerHTML = '<b>Click on any bar to see the recorded rollouts!</b>';
-  videoContainer.appendChild(initialMessage);
 }
 
 // Use DOMContentLoaded instead of $(document).ready for faster execution
